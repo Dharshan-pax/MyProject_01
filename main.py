610558864d4871c737c215ff8e08a562ee30df57
Dharsh <dharshan944@outlook.com>
2026-06-08 10:58:02 +0530

    Initialize Streamlit app with sidebar menu

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+st.title("My Live Application")
+with st.sidebar:
+data = option_menu(
+  menu_title = "My Apps",
+  options=["Home","About","Serives"],
+)
+
