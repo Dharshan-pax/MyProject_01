460a25c8a4310d0fde3fb94d7046cf0d3024d776
Dharsh <dharshan944@outlook.com>
2026-06-08 11:22:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 st.title("My Live Application")
 with st.sidebar:
-data = option_menu(
-  menu_title = "My Apps",
-  options=["Home","About","Serives"],
+    data = option_menu(
+    menu_title = "My Apps",
+    options=["Home","About","Serives"],
 )
 
